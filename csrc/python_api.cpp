b1d6c795eba5a34b3984c8409ffdb388b41ecaba
PGFLMG <1106310035@qq.com>
2025-08-05 01:01:14 +0800

    Update python_api.cpp

--- csrc/python_api.cpp
+++ csrc/python_api.cpp
@@ -16,6 +16,12 @@
 #include "jit_kernels/impls/sm100_fp8_gemm_1d2d.hpp"
 #include "jit_kernels/impls/smxx_layout.hpp"
 
+#define _CONCAT(A, B) A##B
+#define CONCAT(A, B) _CONCAT(A, B)
+
+#define _STRINGIFY(A) #A
+#define STRINGIFY(A) _STRINGIFY(A)
+
 #define REGISTER_EXTENSION(NAME)                                                                      \
   PyMODINIT_FUNC CONCAT(PyInit_, NAME)() {                                                            \
     static struct PyModuleDef module = {PyModuleDef_HEAD_INIT, STRINGIFY(NAME), nullptr, 0, nullptr}; \
